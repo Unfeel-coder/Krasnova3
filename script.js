077baa9257d8d2d9773b610b9191c232305bed8e
Unfeel-coder <galex.ind@gmail.com>
2025-01-21 17:41:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const submitButton = document.getElementById("submit-button");
 const oneDayInMilliseconds = 86400000;
 const oneYearInMilliseconds = 31557600000;
 
+let contractDate;
+let incidentDate;
+
 submitButton.addEventListener("click", () => {
-  const contractDate = new Date(contractDateInput.value);
-  const incidentDate = new Date(incidentDateInput.value);
+  contractDate = new Date(contractDateInput.value);
+  incidentDate = new Date(incidentDateInput.value);
 
   if (incidentDate < new Date(contractDate.getTime() + oneDayInMilliseconds) || incidentDate > new Date(contractDate.getTime() + oneYearInMilliseconds)) {
     console.log("Срок полиса истек или не вступил в силу, проверьте правильность заполнения даты");
